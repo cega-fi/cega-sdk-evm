1f499f125e3da047ccac4a8046978cefa110df78
vsai <vishalsai@cega.fi>
2023-10-30 23:58:56 +0800

    cp_abis: add copying the IDCSEntry contract

--- cp_abis.sh
+++ cp_abis.sh
@@ -1,3 +1,4 @@
+# EVM SDK V1
 path_to_cega_eth_v1="../cega-eth-v1/artifacts/contracts"
 
 echo "Creating directories"
@@ -17,3 +18,13 @@ for viewer_abi in "${viewer_abis[@]}"
 do
   cp "${path_to_cega_eth_v1}/viewers/${viewer_abi}.sol/${viewer_abi}.json" ./src/abi/viewers
 done
+
+# EVM SDK V2
+echo "Starting copying ABIs for v2"
+path_to_cega_eth_v2="../cega-eth-v2/artifacts/contracts"
+path_to_abis_v2="./src/abiV2"
+mkdir -p "${path_to_abis_v2}"
+
+cp "${path_to_cega_eth_v2}/cega-strategies/dcs/interfaces/IDCSEntry.sol/IDCSEntry.json" ${path_to_abis_v2}
+
+echo "Completed copying ABIs for v2"
