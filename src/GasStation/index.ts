209797c9ccb6ecdac6163e9162a4f7b4fe8bf02e
Alick <alickrxu@gmail.com>
2023-12-05 15:52:59 -0800

    Create Infura gas stations

--- src/GasStation/index.ts
+++ src/GasStation/index.ts
@@ -3,11 +3,15 @@ import { EthereumAlchemyGasStation } from './EthereumAlchemyGasStation';
 import { EthereumEtherscanGasStation } from './EthereumEtherscanGasStation';
 import { PolygonGasStation } from './PolygonGasStation';
 import { ArbitrumAlchemyGasStation } from './ArbitrumAlchemyGasStation';
+import { ArbitrumInfuraGasStation } from './ArbitrumInfuraGasStation';
+import { EthereumInfuraGasStation } from './EthereumInfuraGasStation';
 
 export {
   GasStation,
   EthereumAlchemyGasStation,
   EthereumEtherscanGasStation,
+  EthereumInfuraGasStation,
   PolygonGasStation,
   ArbitrumAlchemyGasStation,
+  ArbitrumInfuraGasStation,
 };
