5a4c451d3d94e6937e4aad09610e0796b9435699
Prosenjit Chowdhury <pc.chy007@gmail.com>
2024-09-03 19:00:29 +0600

    bumped up dynamic gas limit buffer for dcs deposits

--- src/sdkV2.ts
+++ src/sdkV2.ts
@@ -677,7 +677,7 @@ export default class CegaEvmSDKV2 {
           [productId, amount, await this._signer.getAddress(), rotationStrategyParams],
           this._signer,
           overrides,
-          50,
+          80,
         )),
         value: asset === ethers.constants.AddressZero ? amount : 0,
       },
